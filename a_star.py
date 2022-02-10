95434237113f7672c2be475f74150e8b1f053543
Rachel Conforti <50918318+rconforti97@users.noreply.github.com>
2022-02-10 18:16:36 -0500

    Working Code but no comments
    Code is working but is very slow. And no comments

--- a_star.py
+++ a_star.py
@@ -63,7 +63,7 @@ def printSolveableBoards(boards):
     return temp_boards
 
 def setPath(current, path, openList):
-    while current.parent != '': # so this is why 4 isnt printing since its the goal board it has no parent
+    while current.parent != '':
         path.insert(0, current.parent.value)
         current = current.parent
     if len(openList) == 1:
@@ -73,10 +73,10 @@ def printPath(path):
     print("Start")
     for c in path: 
         for x in c:
-            for n in x: 
+            for n in x:
                 print(n, end=" ")
             print("")
-        print("")
+        print('  \u2193')
 
 #######################################################################################
 # I am just doing number of misplaced tiles as a heuritstic 
@@ -87,32 +87,20 @@ def heuristic(startNode, goalNode):
             start_x, start_y = np.where(startNode == startNode[i][j])
             goal_x, goal_y = np.where(goalNode == startNode[i][j])
             temp_value += (abs(start_x - goal_x) + abs(start_y - goal_y))
-            
-            # if startNode[i][j] != goalNode[i][j] and startNode[i][j] != 0:
-            #     temp_value += 1
     return temp_value
 
-# -------------------
-# | 0,0 | 0,1 | 0,2 |
-# | 1,0 | 1,1 | 1,2 |
-# | 2,0 | 2,1 | 2,2 |
-# -------------------
 
 def getNeighbors(board):
-    x, y = np.where(board == 0) # getting the index of zero
-    
-    # print('\nboards in board', board)
+    x, y = np.where(board == 0)
     x = int(x)
     y = int(y)
     
-    # print(x,y)
     neighbors = []
     
     # up to be in bounds have to be larger then -1 (x value)
     up_temp = x
     up_temp -= 1 
     if up_temp > -1:
-        # print('up')
         temp_board_up = board.copy()
         temp_spot_up = temp_board_up[x-1][y]
         temp_board_up[x-1][y] = temp_board_up[x][y]
@@ -123,7 +111,6 @@ def getNeighbors(board):
     right_temp = y
     right_temp += 1
     if right_temp < 3 and right_temp > 0 and y != 2:
-        # print('right')
         temp_board_right = board.copy()
         temp_spot_right = temp_board_right[x][y+1]
         temp_board_right[x][y+1] = temp_board_right[x][y]
@@ -134,7 +121,6 @@ def getNeighbors(board):
     down_temp = x
     down_temp += 1 
     if down_temp < 3 and x != 2:
-        # print('down')
         temp_board_down = board.copy()
         temp_spot_down = temp_board_down[x+1][y]
         temp_board_down[x+1][y] = temp_board_down[x][y]
@@ -152,20 +138,14 @@ def getNeighbors(board):
         neighbors.append(temp_board_left)
         
     neighbors = np.array(neighbors)
-    # print('neighbors\n', neighbors)
     return neighbors
 
 
 
 def inList(board, passed_list):
-    print('**************************')
-    # print('board\n', board.value)
     for i in passed_list:
-        # print('i\n', i.value, '\n')
         if np.array_equal(i.value, board.value) == True:
-            print('found a matching board')
             return True
-    print('no matching board ')
     return False
 
 def expandNode(node, openList, openListCopy, closedList, goal):
@@ -179,17 +159,12 @@ def expandNode(node, openList, openListCopy, closedList, goal):
             openListCopy.append(nd)
             break
         
-        # print('closed/open list check')
-        if not inList(nd, closedList) and not inList(nd, openListCopy):
-            print('entered if in EN')
+        if not inList(nd, closedList) == True and not inList(nd, openListCopy) == True:
             openList.put(nd)
             openListCopy.append(nd)
         
         elif inList(nd, openListCopy):
-            print('entered elif in EN')
-            print('nd.g | node.g', nd.g, node.g)
             if nd.g < node.g:
-                print('i got past the child g node check')
                 openList.put(nd)
                 openList.remove(node)
                 openListCopy.append(nd)
@@ -205,22 +180,17 @@ def aStar(start, goal):
     closedList = []
     numExpanded = 0
 
-    print(start)
-    # test = 0 
-    # while test < 1000:
+    # print(start)
     while current.h != 0:
-        # test += 1
         current = openList.get()
         openListCopy.append(current)
         closedList.append(current)
         
-        print('current\n', current.value, 'current.h', current.h)
-        # input("")
         expandNode(current, openList, openListCopy, closedList, goal)
         numExpanded += 1
-        openListCopy.sort(key = lambda node: node.f, reverse=False)
         
-        print('Number of states expanded:', numExpanded)
+        if numExpanded and numExpanded % 100 == 0:
+            print('Expanding nodes - one moment please')
         
     path.append(current.value) # showin it actualy finds goal
     setPath(current, path, openListCopy)
@@ -238,43 +208,12 @@ def main():
     
     goal = [[1, 2, 3],[4, 5, 6],[7, 8, 0]]
     
-    aStar(boards[5], goal)
+    for i in boards:
+        print("\n****************************\nHit enter to solve board:\n", i)
+        input("")
+        aStar(i, goal)
+    # aStar(boards[6], goal)
 # 
 if __name__ == '__main__':
     main()
     print('\nExiting normally. Thank you')
-    
-# =============================================================================
-# Why wont board 5, 6, 7 work?
-#      they are looping for some reason 
-#      idk why its doing this. 
-# So, I think its passing boards 0-4 because its not relying on the on the 
-# search function to figure it out. However, boards 5, 6, 7 are and its always 
-# returning false no matter what. I am not sure why this is happneing 
-# =============================================================================
-
-# old expand node 
-# print('node.value', node.value)
-# children = getNeighbors(node.value)
-# for c in children:
-#     child_g = 1 + node.g
-#     nd = Node(c, node, heuristic(c, goal), child_g) # value, parent, h, g
- # print('inList Result:', inList(nd, openListCopy))
-   # if inList(nd, openListCopy):
-   #     # print('I am in here')
-   #     new_g = nd.g
-   #     old_g = node.g
-   #     if new_g < old_g:
-   #         # print('I am in new_g < old_g')
-   #         openList.put(nd)
-   #         openList.pop(node)
-   #         openListCopy.append(nd)
-   #         openListCopy.remove(node)
-   #         if inList(nd, closedList):
-   #             print("i am in inList(nd, closedList)")
-   #             closedList.remove(node)
-             
-   # elif not inList(nd, closedList):
-   #     # print("i am in not inList(nd, closedList)")
-   #     openList.put(nd)
-   #     openListCopy.append(nd)
